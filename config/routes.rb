b490460756e1741d407a5fe3d62a0c5f5faf7bcd
qinmingyuan <mingyuan0715@foxmail.com>
2025-09-11 20:48:21 +0800

    r

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  scope RailsCom.default_routes_scope do
+  namespace :douyin, defaults: { business: 'douyin' } do
+    resources :apps
 
-    namespace :douyin, defaults: { business: 'douyin' } do
-      resources :apps
-
-      namespace :panel, defaults: { namespace: 'panel' } do
-        resources :apps do
-          resources :shops
-        end
+    namespace :panel, defaults: { namespace: 'panel' } do
+      resources :apps do
+        resources :shops
       end
     end
-
   end
 end
