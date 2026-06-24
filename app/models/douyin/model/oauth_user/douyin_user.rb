3cae47a3049b30c5a7100fd828bb6afa23dd28a4
qinmingyuan <mingyuan0715@foxmail.com>
2026-06-24 17:59:09 +0800

    app lid

--- app/models/douyin/model/oauth_user/douyin_user.rb
+++ app/models/douyin/model/oauth_user/douyin_user.rb
@@ -3,9 +3,17 @@ module Douyin
     extend ActiveSupport::Concern
 
     included do
+      belongs_to :app, foreign_key: :appid, primary_key: :appid, optional: true
 
+      before_validation :sync_from_app, if: -> { appid.present? && appid_changed? }
     end
 
+    def sync_from_app
+      if app
+        self.organ_id = app.organ_id
+        self.app_name = app.name
+      end
+    end
 
   end
 end
